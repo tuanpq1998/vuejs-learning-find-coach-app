bd823497285058785587a1108127ceb4cf9a0095
tuanpq1998 <tuanquang123321@gmail.com>
2021-08-14 16:32:28 +0700

    deploy w firebase

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import {createRouter, createWebHistory} from "vue-router";
-import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import { defineAsyncComponent } from "@vue/runtime-core";
+// import CoachDetail from "./pages/coaches/CoachDetail.vue";
 import CoachesList from "./pages/coaches/CoachesList.vue";
-import CoachRegister from "./pages/coaches/CoachRegister.vue";
-import ContactCoach from "./pages/requests/ContactCoach.vue";
-import RequestList from "./pages/requests/RequestList.vue";
+// import CoachRegister from "./pages/coaches/CoachRegister.vue";
+// import ContactCoach from "./pages/requests/ContactCoach.vue";
+// import RequestList from "./pages/requests/RequestList.vue";
 import NotFound from "./pages/NotFound.vue"
 import UserAuth from "./auth/UserAuth.vue"
 import store from "./store/index"
+
+const CoachDetail = defineAsyncComponent(() => import("./pages/coaches/CoachDetail.vue"));
+const CoachRegister = defineAsyncComponent(() => import("./pages/coaches/CoachRegister.vue"));
+const RequestList = defineAsyncComponent(() => import("./pages/requests/RequestList.vue"));
+const ContactCoach = defineAsyncComponent(() => import("./pages/requests/ContactCoach.vue"));
 
 const router = createRouter({
   history : createWebHistory(),
